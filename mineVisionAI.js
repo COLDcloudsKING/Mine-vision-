480788b5289d61887702bd8f2f57cee0d7e92049
COLDcloudsKING <mqkkg34@gmail.com>
2026-05-31 15:04:58 +0100

    Update mineVisionAI.js

--- mineVisionAI.js
+++ mineVisionAI.js
@@ -2,6 +2,17 @@ class MineVisionAI {
   constructor() {
     this.tileStats = {};
   }
+
+  updateFromScreenshots(screenshots) {
+    this.tileStats.lastScreenshots = screenshots;
+  }
+
+  getPredictionGrid() {
+    return {
+      message: "AI running successfully",
+      predictions: []
+    };
+  }
 }
 
 module.exports = MineVisionAI;
