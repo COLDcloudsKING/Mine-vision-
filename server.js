3d833d9e4c1841025341b67da10c06184093509e
COLDcloudsKING <mqkkg34@gmail.com>
2026-06-02 17:23:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
- 
-const express = require("express");
+ const express = require("express");
 const cors = require("cors");
+const { generateRecommendation } = require("./services/recommendationEngine");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -10,7 +11,12 @@ app.get("/", (req, res) => {
   res.send("Mine Vision is running successfully 🚀");
 });
 
+app.get("/test", (req, res) => {
+  res.json({ message: "AI module loaded successfully" });
+});
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
