3e94e839437c24e78162766d589a523215c49a7b
COLDcloudsKING <mqkkg34@gmail.com>
2026-06-04 23:32:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.get("/test", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+
+app.get("/analyze", (req, res) => {
+  const result = analyzeScreenshots([]);
+  res.json(result);
 });
