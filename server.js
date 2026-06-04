5d4779451309b1e27e52b0e7019a1689a76d5280
COLDcloudsKING <mqkkg34@gmail.com>
2026-06-04 23:04:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { generateRecommendation } = require("./recommendationEngine");
-
+const { analyzeScreenshots } = require("./services/imageAnalyzer");
 const app = express();
 
 app.use(cors());
