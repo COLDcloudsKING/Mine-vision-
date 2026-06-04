0d32ec913fca2539845cfd90a87170ec0d13ae23
COLDcloudsKING <mqkkg34@gmail.com>
2026-06-04 22:42:26 +0100

    Update imageAnalyzer.js

--- imageAnalyzer.js
+++ imageAnalyzer.js
@@ -1,1 +1,12 @@
+function analyzeScreenshots(screenshots) {
+    return {
+        supported: true,
+        mineSetting: 5,
+        screenshotCount: screenshots.length,
+        status: "analysis_received"
+    };
+}
 
+module.exports = {
+    analyzeScreenshots
+};
