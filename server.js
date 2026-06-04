78232c77c7342bbf15e31e0fd8ac1e887ed5dbd1
COLDcloudsKING <mqkkg34@gmail.com>
2026-06-04 14:24:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const { generateRecommendation } = require("./recommendationEngine");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Mine Vision is running successfully 🚀");
+  res.send("Mine Vision is running successfully 🚀");
 });
 
 app.get("/test", (req, res) => {
-    res.json(generateRecommendation());
+  res.json(generateRecommendation());
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT);
+  console.log("Server is running on port " + PORT);
 });
